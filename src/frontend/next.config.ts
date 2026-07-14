bc22f931d244d8a67042ec09bd638554ab1c6de5
JHONATAN RAUL FERNANDEZ SILVA <PE01026884@prosegur.com>
2026-07-14 16:42:43 -0500

    se agrego el acceso al api

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -9,7 +9,8 @@ const nextConfig: NextConfig = {
       { protocol: "http", hostname: "127.0.0.1", port: "5036" },
       { protocol: "https", hostname: "127.0.0.1", port: "5036" },
       { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" }
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "sefexcorpsist.sefexcorp.com", port: "2000" },
     ]
   },
   experimental: {
